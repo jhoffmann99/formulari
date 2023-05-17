b2db16839a1d3e3cb3dfd367c234b832d7a1323b
jhoffmann99 <jhoffmann91@protonmail.com>
2023-05-17 10:51:03 +0200

    ci: jwt config

--- src/main/java/io/jhoffmann/formulari/auth/JwtUtils.java
+++ src/main/java/io/jhoffmann/formulari/auth/JwtUtils.java
@@ -18,13 +18,13 @@ import io.jsonwebtoken.*;
 public class JwtUtils {
   private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-  @Value("${bezkoder.app.jwtSecret}")
+  @Value("${app.jwtSecret}")
   private String jwtSecret;
 
-  @Value("${bezkoder.app.jwtExpirationMs}")
+  @Value("${app.jwtExpirationMs}")
   private int jwtExpirationMs;
 
-  @Value("${bezkoder.app.jwtCookieName}")
+  @Value("${app.jwtCookieName}")
   private String jwtCookie;
 
   public String getJwtFromCookies(HttpServletRequest request) {
