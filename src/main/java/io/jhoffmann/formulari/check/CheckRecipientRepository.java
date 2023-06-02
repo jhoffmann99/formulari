933a5e2b03652d5671ebc935a53de8acc2c856fd
jhoffmann99 <jhoffmann91@protonmail.com>
2023-06-02 09:12:32 +0200

    feat: Order of Checkrecipients

--- src/main/java/io/jhoffmann/formulari/check/CheckRecipientRepository.java
+++ src/main/java/io/jhoffmann/formulari/check/CheckRecipientRepository.java
@@ -10,12 +10,12 @@ import org.springframework.data.repository.query.Param;
 public interface CheckRecipientRepository extends JpaRepository<CheckRecipientEntity, Long> {
     Optional<CheckRecipientEntity> findByUid(String uid);
 
-    @Query("SELECT r FROM CheckRecipientEntity r LEFT JOIN r.check c LEFT JOIN c.user u WHERE c.status = 'REQUESTED' AND r.data is not null AND u.username = :username")
+    @Query("SELECT r FROM CheckRecipientEntity r LEFT JOIN r.check c LEFT JOIN c.user u WHERE c.status = 'REQUESTED' AND r.data is not null AND u.username = :username ORDER BY r.createdAt DESC")
     List<CheckRecipientEntity> findReplies(@Param("username") String username);
 
-    @Query("SELECT r FROM CheckRecipientEntity r LEFT JOIN r.check c LEFT JOIN c.user u WHERE c.status = 'REQUESTED' AND r.data is null AND u.username = :username")
+    @Query("SELECT r FROM CheckRecipientEntity r LEFT JOIN r.check c LEFT JOIN c.user u WHERE c.status = 'REQUESTED' AND r.data is null AND u.username = :username ORDER BY r.createdAt DESC")
     List<CheckRecipientEntity> findOpenReplies(@Param("username") String username);
 
-    @Query("SELECT r FROM CheckRecipientEntity r LEFT JOIN r.check c LEFT JOIN c.user u WHERE c.status = 'ARCHIVED' AND u.username = :username")
+    @Query("SELECT r FROM CheckRecipientEntity r LEFT JOIN r.check c LEFT JOIN c.user u WHERE c.status = 'ARCHIVED' AND u.username = :username ORDER BY r.createdAt DESC")
     List<CheckRecipientEntity> findArchivedReplies(@Param("username") String username);
 }
