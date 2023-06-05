dfdbe484c0265634d4511eb18cc5781be5156361
jhoffmann99 <jhoffmann91@protonmail.com>
2023-06-05 20:07:01 +0200

    feat: asdf

--- src/main/java/io/jhoffmann/formulari/auth/JwtUtils.java
+++ src/main/java/io/jhoffmann/formulari/auth/JwtUtils.java
@@ -38,7 +38,7 @@ public class JwtUtils {
 
   public ResponseCookie generateJwtCookie(UserDetailsImpl userPrincipal) {
     String jwt = generateTokenFromUsername(userPrincipal.getUsername());
-    ResponseCookie cookie = ResponseCookie.from(jwtCookie, jwt).path("/").sameSite("none").secure(true).maxAge(24 * 60 * 60).httpOnly(false).build();
+    ResponseCookie cookie = ResponseCookie.from(jwtCookie, jwt).path("/").sameSite("None").secure(true).maxAge(24 * 60 * 60).httpOnly(false).build();
     return cookie;
   }
 
